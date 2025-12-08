eed8c861255eac0a1b4255cf11376f3d0af14c87
TRAPPIST <in@gav.cloud>
2025-12-08 12:05:59 -0800

    added music.latte layout

--- site/templates/layouts/default.php
+++ site/templates/layouts/default.php
@@ -1,2 +1,3 @@
 <?= $rockfrontend->renderIf("sections/about.latte", "name=about") ?>
+<?= $rockfrontend->renderIf("sections/music.latte", "name=music") ?>
 <?= $rockfrontend->renderIf("sections/promailer-subscribe.latte", "name=newsletter") ?>
