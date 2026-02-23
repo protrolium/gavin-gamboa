6f6d7b55809e53645b1fa6722124f196d72e8a0a
TRAPPIST <in@gav.cloud>
2026-02-23 14:08:26 -0500

    set defaults

--- site/templates/layouts/default.php
+++ site/templates/layouts/default.php
@@ -1,3 +1,4 @@
 <?= $rockfrontend->renderIf("sections/about.latte", "name=about") ?>
 <?= $rockfrontend->renderIf("sections/music.latte", "name=music") ?>
 <?= $rockfrontend->renderIf("sections/promailer-subscribe.latte", "name=newsletter") ?>
+<?= $rockfrontend->renderIf("sections/contact-form.latte", "name=contact-form") ?>
