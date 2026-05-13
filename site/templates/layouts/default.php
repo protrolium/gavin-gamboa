d730c2837f9c09b8774d76a101b6704e9d1377b3
TRAPPIST <in@gav.cloud>
2026-05-12 17:38:40 -0700

    new page layout rules, including moving /newsletter to /subscribe for promailer template

--- site/templates/layouts/default.php
+++ site/templates/layouts/default.php
@@ -2,4 +2,7 @@
 <?= $rockfrontend->renderIf("sections/music.latte", "name=music") ?>
 <?= $rockfrontend->renderIf("sections/media.latte", "name=media") ?>
 <?= $rockfrontend->renderIf("sections/promailer-subscribe.latte", "name=newsletter") ?>
+<?= $rockfrontend->renderIf("sections/promailer-subscribe.latte", "name=subscribe") ?>
 <?= $rockfrontend->renderIf("sections/contact.latte", "name=contact") ?>
+<?= $rockfrontend->renderIf("sections/default-page.latte", "name=ai-policy|colophon|support") ?>
+<?= $rockfrontend->renderIf("sections/sitemap.latte", "name=sitemap") ?>
