d18ca341d5a8c456e3119141e920f44e29ddc150
TRAPPIST <in@gav.cloud>
2026-07-28 00:09:22 -0400

    added rules for events sections

--- site/templates/layouts/default.php
+++ site/templates/layouts/default.php
@@ -1,6 +1,7 @@
 <?= $rockfrontend->renderIf("sections/about.latte", "name=about") ?>
 <?= $rockfrontend->renderIf("sections/music.latte", "name=music") ?>
 <?= $rockfrontend->renderIf("sections/media.latte", "name=media") ?>
+<?= $rockfrontend->renderIf("sections/events.latte", "name=events") ?>
 <?= $rockfrontend->renderIf("sections/promailer-subscribe.latte", "name=newsletter") ?>
 <?= $rockfrontend->renderIf("sections/promailer-subscribe.latte", "name=subscribe") ?>
 <?= $rockfrontend->renderIf("sections/contact.latte", "name=contact") ?>
